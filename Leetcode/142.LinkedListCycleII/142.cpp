e21fb18d43513f8437e9602820daf11f186f4f9e
Neoustc <863177146@qq.com>
2015-12-21 20:06:30 -0500

    Create 142.cpp

---
+++ Leetcode/142.LinkedListCycleII/142.cpp
@@ -0,0 +1,18 @@
+ ListNode *detectCycle(ListNode *head) {
+    if (head == NULL || head -> next == head) return head;
+   	ListNode* slow = head, *fast = head;
+   	while (fast -> next && fast -> next -> next)
+   	{
+   		slow = slow -> next;
+   		fast = fast -> next -> next;
+   		if (slow == fast) break;
+   	}
+   	if (!fast -> next || !fast -> next -> next ) return NULL;
+   	fast = head;
+   	while (fast != slow )
+   	{
+   		fast = fast -> next;
+   		slow = slow -> next;
+   	}
+   	return slow;
+   }
