e1853d2202ebe4930fe468a6e68b29e61ddf7102
Neoustc <863177146@qq.com>
2015-12-25 22:29:49 -0500

    Create 17.cpp

---
+++ Leetcode/17.phoneNum/17.cpp
@@ -0,0 +1,30 @@
+class Solution{
+    public:
+	static string letter[] ;
+	 vector<string> letterCombinations(string digits) {
+	 	int n = digits.size();
+	 	vector<string> res;
+	 	string s = "";
+	 	if (!n) return res;
+	 	helper(res, s, digits, 0);
+	 	return res;
+}
+	void helper(vector<string>& res, string& s, string& digits, int start)
+	{
+		if (start == digits.size())
+		{
+			res.push_back(s);
+			return;
+		}
+		for (int i = 0; i < letter[digits[start] - '0'].size(); ++i)
+		{
+			s += letter[digits[start] - '0'][i];
+			helper(res, s, digits, start + 1);
+			s.pop_back(); 
+		}
+	}
+};
+string Solution :: letter[] = {" ", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+***********************************************
+
