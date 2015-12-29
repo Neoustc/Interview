9c4a4f8ebd1596c90da047a12773ad5332ff49b2
Lei <863177146@qq.com>
2015-12-29 15:56:05 -0500

    Update 209.cpp

--- Leetcode/209.minimumSizeSubarraySum/209.cpp
+++ Leetcode/209.minimumSizeSubarraySum/209.cpp
@@ -1,3 +1,7 @@
+//it's like a slifing window solution;
+//because the inner while loop executes at most once for each starting position ;
+// the inner while loop runs at most N times during the whole for loop from 0 to nums.size() - 1; 
+           
 int minSubArrayLen(int s, vector<int>& nums) {
         int n = nums.size();
         int l = 0, sum = 0;
@@ -6,7 +10,7 @@ int minSubArrayLen(int s, vector<int>& nums) {
     {
         if (nums[r] >= s) return 1;
         sum += nums[r];
-        while (sum >= s) {
+        while (sum >= s) {//try to find the mininum length.keep decreasing the length until the length < s;
             length = min(length, r - l + 1);
             sum -= nums[l++];
         }
