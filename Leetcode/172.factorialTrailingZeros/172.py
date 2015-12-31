c76c70129076e30c28c8fd2ae5e784829fc3fca2
Lei <863177146@qq.com>
2015-12-31 14:56:06 -0500

    Update 172.py

--- Leetcode/172.factorialTrailingZeros/172.py
+++ Leetcode/172.factorialTrailingZeros/172.py
@@ -1,4 +1,11 @@
 class Solution(object):
+     def trailingZeroes(self, n):
+        r = 0
+        while n > 0:
+            n /= 5
+            r += n
+        return r
+        
     def trailingZeroes(self, n):
         """
         :type n: int
