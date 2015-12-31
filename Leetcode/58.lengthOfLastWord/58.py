b9cbc4820cbc55a53f80a9bd38be7130e1fb51a9
Lei <863177146@qq.com>
2015-12-31 14:38:53 -0500

    Create 58.py

---
+++ Leetcode/58.lengthOfLastWord/58.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = s.split()
+        return 0 if len(l) == 0 else len(l[-1]);
+        
