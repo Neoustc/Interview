1d671d6383ba4fd6e5c2050d00d3de076af1fae9
Lei <863177146@qq.com>
2016-01-01 17:24:33 -0500

    Create tle.cpp

---
+++ Leetcode/139.wordBreak/tle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   bool wordBreak(string s, unordered_set<string>& wordDict) {
+    if(wordDict.size()==0)return false;
+ 	int n = s.size();
+ 	if (n == 0 || wordDict.count(s)) return true;
+ 	for (int i = 0; i < n; ++i)
+ 	{
+ 		if (wordDict.count(s.substr(0, i + 1)) > 0)
+ 		{
+ 			if (wordBreak(s.substr(i + 1), wordDict))return true;
+ 		}
+ 	}
+ 	return false;
+   }
+};
