a8760f7d81e95c549345500ab3dec27e831049a2
Lei <863177146@qq.com>
2016-01-01 22:17:57 -0500

    Create 88.cpp

---
+++ Leetcode/88.mergeSortedArray/88.cpp
@@ -0,0 +1,9 @@
+ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+ {
+  int i = m - 1, j = n - 1;
+  int k = m + n -1;
+  while (j >= 0) //only need to consider J we overwrite in the vector nums1.
+  {
+    nums1[k--] = i >= 0 && nums1[i] >= nums2[j] ? nums1[i--] : nums2[j--];
+  }
+ }
