dd65200d9f54c24b3c00112ae25f89345e765762
Lei <863177146@qq.com>
2016-01-06 14:55:22 -0500

    Rename 235.cpp to iterator.cpp

---
+++ Leetcode/235.lowestCommonAncestorOfABinarySearchTree/iterator.cpp
@@ -0,0 +1,6 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+ {
+ 	while ( (p -> val - root -> val) * (q -> val - root -> val) > 0)
+ 		root = p -> val < root -> val ? root -> left : root -> right;
+ 		return root;
+ }
