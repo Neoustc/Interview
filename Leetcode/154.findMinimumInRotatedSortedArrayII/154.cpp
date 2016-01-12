a20ccc3f6537f95a51d5ee1edcedfc77538554d8
Lei <863177146@qq.com>
2016-01-12 16:31:54 -0500

    Create 154.cpp

---
+++ Leetcode/154.findMinimumInRotatedSortedArrayII/154.cpp
@@ -0,0 +1,14 @@
+int findMin(vector<int>& nums) {
+          int left=0,right=nums.size()-1;
+        int mid=0;
+        while(left<right)
+        {	
+        	mid=left+((right-left)>>1);
+        	if(nums[mid]>nums[right])//we shold compare with right,
+        		left=mid+1;
+        	else if(nums[mid]<nums[right])
+        		right=mid;
+        	else --right;
+        }
+        return nums[left];
+    }
