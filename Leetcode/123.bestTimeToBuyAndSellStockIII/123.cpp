2b94f9ebf074a74d23da7aeba3c4b75508bdce96
Lei <863177146@qq.com>
2016-01-13 22:59:05 -0500

    Create 123.cpp

---
+++ Leetcode/123.bestTimeToBuyAndSellStockIII/123.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/discuss/48151/my-c-solution-o-n-time-o-1-space-8ms
+states[][0]: one buy
+states[][1]: one buy, one sell
+states[][2]: two buys, one sell
+states[][3]: two buy, two sells
+
+int maxProfit(std::vector<int> &prices)
+	{
+	  int states[2][4] = {INT_MIN, 0, INT_MIN, 0}; // 0: 1 buy, 1: one buy/sell, 2: 2 buys/1 sell, 3, 2 buys/sells
+        int len = prices.size(), i, cur = 0, next =1;
+        for(i=0; i<len; ++i)
+        {
+            states[next][0] = max(states[cur][0], -prices[i]);
+            states[next][1] = max(states[cur][1], states[cur][0]+prices[i]);
+            states[next][2] = max(states[cur][2], states[cur][1]-prices[i]);
+            states[next][3] = max(states[cur][3], states[cur][2]+prices[i]);
+            swap(next, cur);
+        }
+        return  states[cur][3];
+	}
