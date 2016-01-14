2615260c793b59709f0e0942d8903068ae261fcc
Lei <863177146@qq.com>
2016-01-13 21:32:59 -0500

    Update 122.cpp

--- Leetcode/122.bestTimeToBuyAndSellStockII/122.cpp
+++ Leetcode/122.bestTimeToBuyAndSellStockII/122.cpp
@@ -1,3 +1,4 @@
+ // You need to find the peak of the price. then use the peak's price minus the valley's price.
  int maxProfit(vector<int>& prices)
   	{
  	int n = prices.size();
@@ -9,14 +10,14 @@
 	{
 		if (prices[i] <= prices[i + 1])
 		{ 
-			while (i + 1 < n && prices[i] <= prices[i + 1]){++i;}
-				maxProfit += prices[i] - minPrices;
+			while (i + 1 < n && prices[i] <= prices[i + 1]){++i;}// check whether the price goes up 
+				maxProfit += prices[i] - minPrices;// meet the peak calculate the profit.
 				if (i + 1 >= n) return maxProfit; 
 		}
 		else
 		{
-			while (i + 1 < n && prices[i] > prices[i + 1]){++i;}
-				minPrices = prices[i];
+			while (i + 1 < n && prices[i] > prices[i + 1]){++i;}// check whether the price goes down.
+				minPrices = prices[i];// remember the current valley‘s price.
 			if (i + 1 >= n) return maxProfit;
 		}
 	} 	
