700cd5abc695c3506654e57075465e7d4734e8f1
Lei <863177146@qq.com>
2016-01-17 14:42:08 -0500

    Create addition.cpp

---
+++ Leetcode/66.plusOne/addition.cpp
@@ -0,0 +1,13 @@
+vector<int> plusOne (vector<int>& digits)
+ {
+ 	int carry = 1, n = digits.size();
+ 	vector<int> sum(n, 0);
+ 	for (int i = n - 1; i >= 0; i--)
+ 	{
+ 		int val = digits[i] + c;
+ 		sum[i] = val % 10;
+ 		c = val / 10;
+ 	}
+ 	if (c) sum.insert(sum.begin(), c);
+ 	return sum;
+  }
