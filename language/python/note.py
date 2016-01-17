68c95057d849d89bc7289350bec68f0daed1e269
Lei <863177146@qq.com>
2016-01-17 15:11:07 -0500

    Update note.py

--- language/python/note.py
+++ language/python/note.py
@@ -43,3 +43,13 @@ zip() in conjunction with the * operator can be used to unzip a list:
  Lambda expressions (sometimes called lambda forms) are used to create anonymous functions. 
  The expression    ** lambda arguments: expression **    yields a function object. 
  The unnamed object behaves like a function object defined with
+
+*********************
+reduce(function, iterable[, initializer])
+Apply function of two arguments cumulatively to the items of sequence, from left to right,so as to reduce the sequence to a single value.
+
+For example, reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]) calculates ((((1+2)+3)+4)+5).
+
+The left argument, x, is the accumulated value and the right argument, y, is the update value from the sequence. 
+If the optional initializer is present, it is placed before the items of the sequence in the calculation, and serves as a default when the sequence is empty. 
+If initializer is not given and sequence contains only one item, the first item is returned.
