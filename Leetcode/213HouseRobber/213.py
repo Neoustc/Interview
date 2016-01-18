26b0bec7ceca3948dab45f1880ec408912cf5bfe
Lei <863177146@qq.com>
2016-01-18 14:56:39 -0500

    Create 213.py

---
+++ Leetcode/213HouseRobber/213.py
@@ -0,0 +1,7 @@
+def rob(self, nums):
+        def rob(nums):
+            now = prev = 0
+            for n in nums:
+                now, prev = max(now, prev + n), now
+            return now
+        return max(rob(nums[len(nums) != 1:]), rob(nums[:-1]))
