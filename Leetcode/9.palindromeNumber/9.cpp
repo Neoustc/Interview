b2432e4dc1db14f4592aaa8a925776ab7d3e5f91
Lei <863177146@qq.com>
2016-01-19 15:05:53 -0500

    Create 9.cpp

---
+++ Leetcode/9.palindromeNumber/9.cpp
@@ -0,0 +1,22 @@
+ maintain two divisors to get the most and least significant digits
+ and compare them.
+ class Solution {
+public:
+   bool isPalindrome (int x) 
+ {
+ 	//if (x < 0) return false;
+ 	//if ( 0 <= x &&  x < 10) return true;
+ 	if (x < 0 || (x && !(x % 10))) return false;
+ 	int p = 0;
+ 	int q = x;
+ 	while (q > 0)
+ 	{
+ 		p = p * 10 + q % 10;
+ 		q = q / 10;
+ 	}
+ 	return x == p;
+ 	//cout << p <<" " << q <<endl;
+ 	//if (x == p) return true;
+ 	//return false;
+ }
+};
