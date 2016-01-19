0f5c89cbe7a707afd4c2231b7b7e8c2623d6755c
Lei <863177146@qq.com>
2016-01-19 16:32:52 -0500

    Create 35.cpp

---
+++ Leetcode/35.searchInsertPosition/35.cpp
@@ -0,0 +1,18 @@
+ //find the target or the first one greater than target.
+ class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+     int n = nums.size();
+ 	int low = 0, high = n;
+ 	int mid = 0;
+ 	// answer is [0,n] or [low, high]
+ 	while (low < high)
+ 	{
+ 		mid = low + ((high - low ) >> 1);
+ 		if (nums[mid] == target) return mid;
+ 		if (nums[mid] < target) low = mid + 1;//[low(mid + 1), high];
+ 		else high = mid;
+ 	}
+ 	return low;
+    }
+};
