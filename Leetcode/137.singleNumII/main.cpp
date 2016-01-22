46c34ec1a10fd3c9e10fba89fc0883e711b2ceb6
Lei <863177146@qq.com>
2016-01-22 16:34:06 -0500

    Create main.cpp

---
+++ Leetcode/137.singleNumII/main.cpp
@@ -0,0 +1,27 @@
+int singleNumber(int arr[], int n) {
+        // Initialize result
+    int result = 0;
+
+    int x, sum;
+
+    // Iterate through every bit
+    for (int i = 0; i < 32; i++)
+    {
+      // Find sum of set bits at ith position in all
+      // array elements
+      sum = 0;
+      x = (1 << i);
+      for (int j=0; j< n; j++ )
+      {
+          if (arr[j] & x)
+            sum++;
+      }
+
+      // The bits with sum not multiple of 3, are the
+      // bits of element with single occurrence.
+      if (sum % 3)
+        result |= x;
+    }
+
+    return result;
+    }
