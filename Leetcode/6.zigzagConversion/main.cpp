9b307265ac891b96f83156d1af981972f2d4490b
Lei <863177146@qq.com>
2016-01-31 12:30:32 -0500

    Update main.cpp

--- Leetcode/6.zigzagConversion/main.cpp
+++ Leetcode/6.zigzagConversion/main.cpp
@@ -1,18 +1,22 @@
-  string convert(string s, int numRows) 
+class Solution {
+public:
+     string convert(string s, int numRows) 
   {
   	int n = s.size();
+  	if(numRows <= 1) return s;
   	string * strs = new string[numRows];
   	int step = numRows;
   	int dir = 1;
   	int pos = 0;
-  	for (int i = 0; i < n; ++i)
+  	for (int i = 0; i < n;)
   	{	
   		int j = 0;
   		if (i < n && dir && j < step)
   		{
   			while (i < n && j < step)
   			{
-	  			strs[pos++] = s[i];
+	  			strs[pos++] += s[i];
+	  			++i;
 	  			j++;
   			}
   			if (i == n) break;
@@ -23,11 +27,12 @@
   			while (i < n && j < step)
   			{
   				j++;
-  				strs[--pos] = s[i];
+  				strs[--pos] += s[i];
+  				++i;
   				
   			}
   			if (i == n) break;
-  			else {dir = 1; pos ++}
+  			else {dir = 1;pos ++;}
   		}
   	}
   	string res;
@@ -36,3 +41,4 @@
   	delete[] strs;
   	return res;
   }
+};
