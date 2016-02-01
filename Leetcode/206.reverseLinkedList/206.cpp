5377eb13da8b19c528dc84e97018ae58720e3dab
Lei <863177146@qq.com>
2016-02-01 11:01:36 -0500

    Create 206.cpp

---
+++ Leetcode/206.reverseLinkedList/206.cpp
@@ -0,0 +1,10 @@
+  ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        while (head) {
+            ListNode* next = head -> next;
+            head -> next = pre;
+            pre = head;
+            head = next;
+        } 
+        return pre;
+    }
