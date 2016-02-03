0873acc87c4247ccc97e8d149f23e2ee3d7792f2
Lei <863177146@qq.com>
2016-02-03 16:51:48 -0500

    Create 300.cpp

---
+++ Leetcode/300.LIS/300.cpp
@@ -0,0 +1,47 @@
+//http://blog.csdn.net/u010738052/article/details/50405882
+//dp[i] = dp[i - 1] + 1       if nums[i] > nums[i - 1];
+dp[i] = max(dp[j]) + 1		if nums[i] <= nums[i - 1] && nums[i] > nums[j];
+
+ int lengthOfLIS(vector<int>& nums) 
+ {
+ 	int n = nums.size();
+ 	if (!n) return 0;
+ 	vector<int> dp(n, 1);
+ 	int res = 1;
+ 	for (int i = 1; i < n; ++i)
+ 	{
+ 	  for (int j = i - 1; j >= 0; j--)
+ 			{
+ 				if (nums[i] > nums[j])
+ 					dp[i] = max(dp[i], dp[j] + 1);
+ 			    res = max(res, dp[i]);
+ 			}
+ 	}
+ 	 return res;
+ }
+____________________________________________
+int lengthOfLIS(vector<int>& nums) 
+{
+	int n = nums.size();
+	if (!n) return 0;
+	vector<int> ends{nums[0]};
+	int res = 1;
+	for (int num : nums)
+	{
+		if (num > ends.back())
+			ends.push_back(num);
+		else if (num < ends[0]) ends[0] = num; 
+		else{
+			int l = 1, r = ends.size() - 1;
+			while (l < r - 1)
+			{
+				int m = l + ((r - l) >> 1);
+				if (ends[m] < num) l = m + 1;
+				else r = m;
+			}
+			ends[l] = num; 
+		}
+	}
+	return ends.size();
+}
+}
