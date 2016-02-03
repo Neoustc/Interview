e2070e4528c9bd553795d4d7ceb91cb9ca428c96
Lei <863177146@qq.com>
2016-02-03 15:38:57 -0500

    Create 299.py

---
+++ Leetcode/299.bullsAndCows/299.py
@@ -0,0 +1,29 @@
+# collections.counter return a dict(key:value)
+#for  counting the hashable obejects 
+#collections.Counter("102")
+#Counter({'1': 1, '0': 1, '2': 1})
+#zip(*iterables)
+#Make an iterator that aggregates elements from each of the iterables.
+#s=Counter({'1': 1, '0': 1, '2': 1}) 
+#d=Counter({'1': 1, '0': 1, '3': 1})
+#zip (s,d)
+#[('1', '1'), ('0', '0'), ('2', '3')]
+#s&d 
+#s&d=Counter({'1': 1, '0': 1})
+#(s&d).values()
+#>>>[1, 1]
+import collections
+class Solution(object):
+    def getHint(self, secret, guess):
+		s, g = Counter(secret), Counter(guess)
+	    a = sum(i == j for i, j in zip(secret, guess)) 
+	    return '%sA%sB' % (a, sum((s & g).values()) - a)
+
+def getHint(self, secret, guess):
+    bulls = sum(map(operator.eq, secret, guess))
+    both = sum(min(secret.count(x), guess.count(x)) for x in '0123456789')
+    return '%dA%dB' % (bulls, both - bulls)
+
+
+
+    
