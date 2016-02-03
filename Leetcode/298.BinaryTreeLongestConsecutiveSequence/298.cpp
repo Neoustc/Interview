8815893c027be6deca02dd3163b7a04c3f086ff5
Lei <863177146@qq.com>
2016-02-02 23:32:30 -0500

    Create 298.cpp

---
+++ Leetcode/298.BinaryTreeLongestConsecutiveSequence/298.cpp
@@ -0,0 +1,39 @@
+Problem Description:
+
+Given a binary tree, find the length of the longest consecutive sequence path.
+
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+
+For example,
+
+   1
+    \
+     3
+    / \
+   2   4
+        \
+         5
+Longest consecutive sequence path is 3-4-5, so return 3.
+
+   2
+    \
+     3
+    / 
+   2    
+  / 
+ 1
+Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+
+**********************************************
+int longestConsecutive(TreeNode* root) 
+{
+  return search(root, NULL, 0);
+}
+
+int search(TreeNode * root, TreeNode *parent, int len)
+{
+  if (!root) return len;
+  len = (parent && root -> val == parent -> val + 1) ? len + 1 : 1;
+  return max(len, max(search(root->left, root, len), search(root->right, root, len)));
+}
+
