d6295a372b0cafa0cde350d3054dd4411be0f0e2
Lei <863177146@qq.com>
2016-02-04 15:07:09 -0500

    Create 303.cpp

---
+++ Leetcode/303.rangeSumQuery-Immutable/303.cpp
@@ -0,0 +1,11 @@
+NumArray(vector<int> &nums) {
+       accu.push_back(0);
+       for (int num : nums)
+       accu.push_back(accu.back() + num);
+    }
+
+    int sumRange(int i, int j) {
+        return accu[j + 1] - accu[i];
+    }
+ private:
+ 	vector<int> accu;
