8515536055e94fba9613904b2c520b4aee4e0b08
Lei <863177146@qq.com>
2016-02-05 20:19:41 -0500

    Update 67.cpp

--- Leetcode/67.addBinary/67.cpp
+++ Leetcode/67.addBinary/67.cpp
@@ -1,4 +1,6 @@
- string addBinary(string a, string b) 
+class Solution {
+public:
+   string addBinary(string a, string b) 
  {
  	int n = a.size(), m = b.size();
  	int carry = 0;
@@ -7,8 +9,10 @@
  	{
  		carry += n ? a[--n] - '0' : 0;
  		carry += m ? b[--m] - '0' : 0;
- 		res = to_string(carry % 2) + res;
+ 		res = to_string(carry % 2) + res; // faster
+ 		//res = char(carry % 2 + '0') + res;
  		carry = carry / 2;
  	}
  	return res;
  }
+};
