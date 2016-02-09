af047c80ebf8a725a18e60809ba2c44cd7291c39
Lei <863177146@qq.com>
2016-02-09 15:27:06 -0500

    Create preorderRecursive.cpp

---
+++ Leetcode/114.binaryTreeTraversal/preorderRecursive.cpp
@@ -0,0 +1,15 @@
+vector<int> preorderTraversal(TreeNode* root) 
+{
+	vector<int> res;
+	helper(root, res); 
+	return res;
+}
+
+void helper(TreeNode * root, vector<int>& res)
+{
+	if (!root) return;
+	res.push_back(root -> val);
+	helper(root -> left, res);
+	helper(root -> right, res);
+	return;
+}
