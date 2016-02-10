7ebcb2d58e4a09e77c6325d0348393ded64500c5
Lei <863177146@qq.com>
2016-02-10 00:03:50 -0500

    Create morrisPreorder.cpp

---
+++ Leetcode/114.binaryTreeTraversal/morrisPreorder.cpp
@@ -0,0 +1,30 @@
+//http://blog.csdn.net/u010738052/article/details/50649605
+vector<int> preorderTraversal(TreeNode* root)
+{
+    TreeNode * cur = root, *predecessor = NULL;
+    vector<int> res;
+    while (cur)
+    {
+        if (!cur -> left)
+        {
+            res.push_back(cur -> val);
+            cur = cur -> right;
+        }
+        else{
+            predecessor = cur -> left;
+            while (predecessor -> right && predecessor -> right != cur)
+                predecessor = predecessor -> right;
+            if (!predecessor -> right)
+            {
+                res.push_back(cur -> val); // difference with inorder, push the value when index is build;
+                predecessor -> right = cur;
+                cur = cur -> left;
+            }
+            else{
+                predecessor -> right = NULL;
+                cur = cur -> right;
+            }
+        }
+    }
+    return res;
+}
