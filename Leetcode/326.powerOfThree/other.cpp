bc60d28f5152fc7367607b8808eb360f28b79004
Lei <863177146@qq.com>
2016-02-13 11:06:42 -0500

    Update other.cpp

--- Leetcode/326.powerOfThree/other.cpp
+++ Leetcode/326.powerOfThree/other.cpp
@@ -2,3 +2,12 @@
  {
    return fmod(log10(n)/log10(3), 1)==0;//Returns the floating-point remainder of numer/denom
  }
+ 
+ class Solution {
+public:
+   bool isPowerOfThree(int n) 
+ {
+  double logRes = log10(n)/log10(3);
+  return (logRes - int(logRes) == 0) ? true : false;
+ }
+};
