ba3ec8f82a7b24cdac24d49b8ed206cace5ae7c0
Lei <863177146@qq.com>
2016-02-17 15:36:48 -0500

    Create 118.py

---
+++ Leetcode/118.pascal-sTriangle/118.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        res=[]
+        for i in range(numRows):
+        	#res += ([1]*(i+1)),
+        	res.append([1] * (i + 1))
+        	if i>1:
+        		for j in range(1,i):
+        			res[i][j]=(res[i-1][j-1]+res[i-1][j])
+        return res
