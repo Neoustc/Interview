e75e7a9d9c66ee225d9eff38d76ad2c0cbef8c36
Lei <863177146@qq.com>
2016-02-19 18:11:56 -0500

    Create 71.cpp

---
+++ Leetcode/71.simplifyPath/71.cpp
@@ -0,0 +1,16 @@
+//getline to split the string;
+string simplifyPath(string path) 
+{
+	int n = path.size();
+	string res, tmp;
+	vector<string> stk;
+	stringstream ss(path);
+	while (getline(ss, tmp, '/'))
+	{
+		if (tmp == "" or tmp == ".") continue;
+		if (tmp == ".." and !stk.empty()) stk.pop_back();
+		else if (tmp != "..") stk.push_back(tmp);
+	}
+	for (auto str : stk) res += "/" + str;
+	return res.empty() ? "/" : res;
+}
