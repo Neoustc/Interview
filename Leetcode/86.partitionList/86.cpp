9add3919d4f6db78e7ccfc184d1377bbcf63782d
Lei <863177146@qq.com>
2016-02-27 14:57:15 -0800

    Create 86.cpp

---
+++ Leetcode/86.partitionList/86.cpp
@@ -0,0 +1,23 @@
+ ListNode* partition(ListNode* head, int x) 
+ {
+ 	ListNode * left = new ListNode (-1);
+ 	ListNode * right = new ListNode (-1);
+ 	ListNode * p = left, * q = right;
+ 	while (head)
+ 	{
+ 		ListNode * next = head -> next;
+ 		head -> next = NULL;
+ 		if (head -> val <= x)
+ 			{
+ 				p -> next = head;
+ 				p = p -> next;
+ 			}
+ 		else {
+ 			q -> next = head;
+ 			q = q ->next;
+ 		}
+ 		head = next;
+ 	}
+ 	p -> next = right -> next;
+ 	return left -> next;
+ }
