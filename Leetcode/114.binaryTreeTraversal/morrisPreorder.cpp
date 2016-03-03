f80fec278da46da226985107f74bfffe3fe1e01c
Lei <863177146@qq.com>
2016-03-03 14:27:37 -0800

    Update morrisPreorder.cpp

--- Leetcode/114.binaryTreeTraversal/morrisPreorder.cpp
+++ Leetcode/114.binaryTreeTraversal/morrisPreorder.cpp
@@ -1,6 +1,37 @@
 //http://blog.csdn.net/u010738052/article/details/50649605
 vector<int> preorderTraversal(TreeNode* root)
 {
+	TreeNode * cur = root, *predecessor = NULL;
+	vector<int> res;
+	while (cur)
+	{
+		if (cur -> left)
+		{
+			predecessor = cur -> left;
+			while (predecessor -> right && predecessor ->right != cur)
+				predecessor = predecessor -> right;
+			if (!predecessor -> right)
+			{
+				res.push_back(cur -> val);
+				predecessor -> right = cur;
+				cur = cur -> left;
+			}
+			else {
+				predecessor -> right = NULL;
+				cur = cur -> right;
+			}
+		}
+		else {
+			res.push_back(cur -> val);
+			cur = cur -> right;
+		}
+	}
+	return res;
+}
+*************************************
+
+vector<int> preorderTraversal(TreeNode* root)
+{
     TreeNode * cur = root, *predecessor = NULL;
     vector<int> res;
     while (cur)
