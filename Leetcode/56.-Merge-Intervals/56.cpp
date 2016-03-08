5ff13969f3a925a38809bfae0cad7fa32f31fc7c
Lei <863177146@qq.com>
2016-03-07 21:30:10 -0800

    Create 56.cpp

---
+++ Leetcode/56.-Merge-Intervals/56.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& ins) {
+    if (ins.empty()) return vector<Interval>{};
+    vector<Interval> res;
+    int n = ins.size();
+    sort(ins.begin(), ins.end(), [](Interval a, Interval b) {return a.start < b.start;});
+    res.push_back(ins[0]);
+    for (int j = 1; j < n; ++j)
+	    {	
+	    	if (res.back().end < ins[j].start) res.push_back(ins[j]);
+	    	else res.back().end = max (res.back().end, ins[j].end);
+	    }
+	    return res;
+}
+   
+};
